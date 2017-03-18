e82c0ba0166126de9dfb8be3bc5a2670e178714d
John Schulman <joschu@openai.com>
2017-03-18 15:37:14 -0700

    more informative error message when logdir exists

--- hw4/logz.py
+++ hw4/logz.py
@@ -47,7 +47,7 @@ def configure_output_dir(d=None):
     Set output directory to d, or to /tmp/somerandomnumber if d is None
     """
     G.output_dir = d or "/tmp/experiments/%i"%int(time.time())
-    assert not osp.exists(G.output_dir)
+    assert not osp.exists(G.output_dir), "Log dir %s already exists! Delete it first or use a different dir"%G.output_dir
     os.makedirs(G.output_dir)
     G.output_file = open(osp.join(G.output_dir, "log.txt"), 'w')
     atexit.register(G.output_file.close)
