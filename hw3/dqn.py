436a00a56a0fc781f4a0f029e39d69e8c57d0cc0
Abhishek Gupta <abhishekunique@gmail.com>
2017-09-19 19:43:19 -0700

    added in HW3

---
+++ hw3/dqn.py
@@ -0,0 +1,264 @@
+import sys
+import gym.spaces
+import itertools
+import numpy as np
+import random
+import tensorflow                as tf
+import tensorflow.contrib.layers as layers
+from collections import namedtuple
+from dqn_utils import *
+
+OptimizerSpec = namedtuple("OptimizerSpec", ["constructor", "kwargs", "lr_schedule"])
+
+def learn(env,
+          q_func,
+          optimizer_spec,
+          session,
+          exploration=LinearSchedule(1000000, 0.1),
+          stopping_criterion=None,
+          replay_buffer_size=1000000,
+          batch_size=32,
+          gamma=0.99,
+          learning_starts=50000,
+          learning_freq=4,
+          frame_history_len=4,
+          target_update_freq=10000,
+          grad_norm_clipping=10):
+    """Run Deep Q-learning algorithm.
+
+    You can specify your own convnet using q_func.
+
+    All schedules are w.r.t. total number of steps taken in the environment.
+
+    Parameters
+    ----------
+    env: gym.Env
+        gym environment to train on.
+    q_func: function
+        Model to use for computing the q function. It should accept the
+        following named arguments:
+            img_in: tf.Tensor
+                tensorflow tensor representing the input image
+            num_actions: int
+                number of actions
+            scope: str
+                scope in which all the model related variables
+                should be created
+            reuse: bool
+                whether previously created variables should be reused.
+    optimizer_spec: OptimizerSpec
+        Specifying the constructor and kwargs, as well as learning rate schedule
+        for the optimizer
+    session: tf.Session
+        tensorflow session to use.
+    exploration: rl_algs.deepq.utils.schedules.Schedule
+        schedule for probability of chosing random action.
+    stopping_criterion: (env, t) -> bool
+        should return true when it's ok for the RL algorithm to stop.
+        takes in env and the number of steps executed so far.
+    replay_buffer_size: int
+        How many memories to store in the replay buffer.
+    batch_size: int
+        How many transitions to sample each time experience is replayed.
+    gamma: float
+        Discount Factor
+    learning_starts: int
+        After how many environment steps to start replaying experiences
+    learning_freq: int
+        How many steps of environment to take between every experience replay
+    frame_history_len: int
+        How many past frames to include as input to the model.
+    target_update_freq: int
+        How many experience replay rounds (not steps!) to perform between
+        each update to the target Q network
+    grad_norm_clipping: float or None
+        If not None gradients' norms are clipped to this value.
+    """
+    assert type(env.observation_space) == gym.spaces.Box
+    assert type(env.action_space)      == gym.spaces.Discrete
+
+    ###############
+    # BUILD MODEL #
+    ###############
+
+    if len(env.observation_space.shape) == 1:
+        # This means we are running on low-dimensional observations (e.g. RAM)
+        input_shape = env.observation_space.shape
+    else:
+        img_h, img_w, img_c = env.observation_space.shape
+        input_shape = (img_h, img_w, frame_history_len * img_c)
+    num_actions = env.action_space.n
+
+    # set up placeholders
+    # placeholder for current observation (or state)
+    obs_t_ph              = tf.placeholder(tf.uint8, [None] + list(input_shape))
+    # placeholder for current action
+    act_t_ph              = tf.placeholder(tf.int32,   [None])
+    # placeholder for current reward
+    rew_t_ph              = tf.placeholder(tf.float32, [None])
+    # placeholder for next observation (or state)
+    obs_tp1_ph            = tf.placeholder(tf.uint8, [None] + list(input_shape))
+    # placeholder for end of episode mask
+    # this value is 1 if the next state corresponds to the end of an episode,
+    # in which case there is no Q-value at the next state; at the end of an
+    # episode, only the current state reward contributes to the target, not the
+    # next state Q-value (i.e. target is just rew_t_ph, not rew_t_ph + gamma * q_tp1)
+    done_mask_ph          = tf.placeholder(tf.float32, [None])
+
+    # casting to float on GPU ensures lower data transfer times.
+    obs_t_float   = tf.cast(obs_t_ph,   tf.float32) / 255.0
+    obs_tp1_float = tf.cast(obs_tp1_ph, tf.float32) / 255.0
+
+    # Here, you should fill in your own code to compute the Bellman error. This requires
+    # evaluating the current and next Q-values and constructing the corresponding error.
+    # TensorFlow will differentiate this error for you, you just need to pass it to the
+    # optimizer. See assignment text for details.
+    # Your code should produce one scalar-valued tensor: total_error
+    # This will be passed to the optimizer in the provided code below.
+    # Your code should also produce two collections of variables:
+    # q_func_vars
+    # target_q_func_vars
+    # These should hold all of the variables of the Q-function network and target network,
+    # respectively. A convenient way to get these is to make use of TF's "scope" feature.
+    # For example, you can create your Q-function network with the scope "q_func" like this:
+    # <something> = q_func(obs_t_float, num_actions, scope="q_func", reuse=False)
+    # And then you can obtain the variables like this:
+    # q_func_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='q_func')
+    # Older versions of TensorFlow may require using "VARIABLES" instead of "GLOBAL_VARIABLES"
+    ######
+    
+    # YOUR CODE HERE
+
+    ######
+
+    # construct optimization op (with gradient clipping)
+    learning_rate = tf.placeholder(tf.float32, (), name="learning_rate")
+    optimizer = optimizer_spec.constructor(learning_rate=learning_rate, **optimizer_spec.kwargs)
+    train_fn = minimize_and_clip(optimizer, total_error,
+                 var_list=q_func_vars, clip_val=grad_norm_clipping)
+
+    # update_target_fn will be called periodically to copy Q network to target Q network
+    update_target_fn = []
+    for var, var_target in zip(sorted(q_func_vars,        key=lambda v: v.name),
+                               sorted(target_q_func_vars, key=lambda v: v.name)):
+        update_target_fn.append(var_target.assign(var))
+    update_target_fn = tf.group(*update_target_fn)
+
+    # construct the replay buffer
+    replay_buffer = ReplayBuffer(replay_buffer_size, frame_history_len)
+
+    ###############
+    # RUN ENV     #
+    ###############
+    model_initialized = False
+    num_param_updates = 0
+    mean_episode_reward      = -float('nan')
+    best_mean_episode_reward = -float('inf')
+    last_obs = env.reset()
+    LOG_EVERY_N_STEPS = 10000
+
+    for t in itertools.count():
+        ### 1. Check stopping criterion
+        if stopping_criterion is not None and stopping_criterion(env, t):
+            break
+
+        ### 2. Step the env and store the transition
+        # At this point, "last_obs" contains the latest observation that was
+        # recorded from the simulator. Here, your code needs to store this
+        # observation and its outcome (reward, next observation, etc.) into
+        # the replay buffer while stepping the simulator forward one step.
+        # At the end of this block of code, the simulator should have been
+        # advanced one step, and the replay buffer should contain one more
+        # transition.
+        # Specifically, last_obs must point to the new latest observation.
+        # Useful functions you'll need to call:
+        # obs, reward, done, info = env.step(action)
+        # this steps the environment forward one step
+        # obs = env.reset()
+        # this resets the environment if you reached an episode boundary.
+        # Don't forget to call env.reset() to get a new observation if done
+        # is true!!
+        # Note that you cannot use "last_obs" directly as input
+        # into your network, since it needs to be processed to include context
+        # from previous frames. You should check out the replay buffer
+        # implementation in dqn_utils.py to see what functionality the replay
+        # buffer exposes. The replay buffer has a function called
+        # encode_recent_observation that will take the latest observation
+        # that you pushed into the buffer and compute the corresponding
+        # input that should be given to a Q network by appending some
+        # previous frames.
+        # Don't forget to include epsilon greedy exploration!
+        # And remember that the first time you enter this loop, the model
+        # may not yet have been initialized (but of course, the first step
+        # might as well be random, since you haven't trained your net...)
+
+        #####
+        
+        # YOUR CODE HERE
+
+        #####
+
+        # at this point, the environment should have been advanced one step (and
+        # reset if done was true), and last_obs should point to the new latest
+        # observation
+
+        ### 3. Perform experience replay and train the network.
+        # note that this is only done if the replay buffer contains enough samples
+        # for us to learn something useful -- until then, the model will not be
+        # initialized and random actions should be taken
+        if (t > learning_starts and
+                t % learning_freq == 0 and
+                replay_buffer.can_sample(batch_size)):
+            # Here, you should perform training. Training consists of four steps:
+            # 3.a: use the replay buffer to sample a batch of transitions (see the
+            # replay buffer code for function definition, each batch that you sample
+            # should consist of current observations, current actions, rewards,
+            # next observations, and done indicator).
+            # 3.b: initialize the model if it has not been initialized yet; to do
+            # that, call
+            #    initialize_interdependent_variables(session, tf.global_variables(), {
+            #        obs_t_ph: obs_t_batch,
+            #        obs_tp1_ph: obs_tp1_batch,
+            #    })
+            # where obs_t_batch and obs_tp1_batch are the batches of observations at
+            # the current and next time step. The boolean variable model_initialized
+            # indicates whether or not the model has been initialized.
+            # Remember that you have to update the target network too (see 3.d)!
+            # 3.c: train the model. To do this, you'll need to use the train_fn and
+            # total_error ops that were created earlier: total_error is what you
+            # created to compute the total Bellman error in a batch, and train_fn
+            # will actually perform a gradient step and update the network parameters
+            # to reduce total_error. When calling session.run on these you'll need to
+            # populate the following placeholders:
+            # obs_t_ph
+            # act_t_ph
+            # rew_t_ph
+            # obs_tp1_ph
+            # done_mask_ph
+            # (this is needed for computing total_error)
+            # learning_rate -- you can get this from optimizer_spec.lr_schedule.value(t)
+            # (this is needed by the optimizer to choose the learning rate)
+            # 3.d: periodically update the target network by calling
+            # session.run(update_target_fn)
+            # you should update every target_update_freq steps, and you may find the
+            # variable num_param_updates useful for this (it was initialized to 0)
+            #####
+            
+            # YOUR CODE HERE
+
+            #####
+
+        ### 4. Log progress
+        episode_rewards = get_wrapper_by_name(env, "Monitor").get_episode_rewards()
+        if len(episode_rewards) > 0:
+            mean_episode_reward = np.mean(episode_rewards[-100:])
+        if len(episode_rewards) > 100:
+            best_mean_episode_reward = max(best_mean_episode_reward, mean_episode_reward)
+        if t % LOG_EVERY_N_STEPS == 0 and model_initialized:
+            print("Timestep %d" % (t,))
+            print("mean reward (100 episodes) %f" % mean_episode_reward)
+            print("best mean reward %f" % best_mean_episode_reward)
+            print("episodes %d" % len(episode_rewards))
+            print("exploration %f" % exploration.value(t))
+            print("learning_rate %f" % optimizer_spec.lr_schedule.value(t))
+            sys.stdout.flush()
---
+++ hw3/dqn_utils.py
@@ -0,0 +1,350 @@
+"""This file includes a collection of utility functions that are useful for
+implementing DQN."""
+import gym
+import tensorflow as tf
+import numpy as np
+import random
+
+def huber_loss(x, delta=1.0):
+    # https://en.wikipedia.org/wiki/Huber_loss
+    return tf.select(
+        tf.abs(x) < delta,
+        tf.square(x) * 0.5,
+        delta * (tf.abs(x) - 0.5 * delta)
+    )
+
+def sample_n_unique(sampling_f, n):
+    """Helper function. Given a function `sampling_f` that returns
+    comparable objects, sample n such unique objects.
+    """
+    res = []
+    while len(res) < n:
+        candidate = sampling_f()
+        if candidate not in res:
+            res.append(candidate)
+    return res
+
+class Schedule(object):
+    def value(self, t):
+        """Value of the schedule at time t"""
+        raise NotImplementedError()
+
+class ConstantSchedule(object):
+    def __init__(self, value):
+        """Value remains constant over time.
+        Parameters
+        ----------
+        value: float
+            Constant value of the schedule
+        """
+        self._v = value
+
+    def value(self, t):
+        """See Schedule.value"""
+        return self._v
+
+def linear_interpolation(l, r, alpha):
+    return l + alpha * (r - l)
+
+class PiecewiseSchedule(object):
+    def __init__(self, endpoints, interpolation=linear_interpolation, outside_value=None):
+        """Piecewise schedule.
+        endpoints: [(int, int)]
+            list of pairs `(time, value)` meanining that schedule should output
+            `value` when `t==time`. All the values for time must be sorted in
+            an increasing order. When t is between two times, e.g. `(time_a, value_a)`
+            and `(time_b, value_b)`, such that `time_a <= t < time_b` then value outputs
+            `interpolation(value_a, value_b, alpha)` where alpha is a fraction of
+            time passed between `time_a` and `time_b` for time `t`.
+        interpolation: lambda float, float, float: float
+            a function that takes value to the left and to the right of t according
+            to the `endpoints`. Alpha is the fraction of distance from left endpoint to
+            right endpoint that t has covered. See linear_interpolation for example.
+        outside_value: float
+            if the value is requested outside of all the intervals sepecified in
+            `endpoints` this value is returned. If None then AssertionError is
+            raised when outside value is requested.
+        """
+        idxes = [e[0] for e in endpoints]
+        assert idxes == sorted(idxes)
+        self._interpolation = interpolation
+        self._outside_value = outside_value
+        self._endpoints      = endpoints
+
+    def value(self, t):
+        """See Schedule.value"""
+        for (l_t, l), (r_t, r) in zip(self._endpoints[:-1], self._endpoints[1:]):
+            if l_t <= t and t < r_t:
+                alpha = float(t - l_t) / (r_t - l_t)
+                return self._interpolation(l, r, alpha)
+
+        # t does not belong to any of the pieces, so doom.
+        assert self._outside_value is not None
+        return self._outside_value
+
+class LinearSchedule(object):
+    def __init__(self, schedule_timesteps, final_p, initial_p=1.0):
+        """Linear interpolation between initial_p and final_p over
+        schedule_timesteps. After this many timesteps pass final_p is
+        returned.
+        Parameters
+        ----------
+        schedule_timesteps: int
+            Number of timesteps for which to linearly anneal initial_p
+            to final_p
+        initial_p: float
+            initial output value
+        final_p: float
+            final output value
+        """
+        self.schedule_timesteps = schedule_timesteps
+        self.final_p            = final_p
+        self.initial_p          = initial_p
+
+    def value(self, t):
+        """See Schedule.value"""
+        fraction  = min(float(t) / self.schedule_timesteps, 1.0)
+        return self.initial_p + fraction * (self.final_p - self.initial_p)
+
+def compute_exponential_averages(variables, decay):
+    """Given a list of tensorflow scalar variables
+    create ops corresponding to their exponential
+    averages
+    Parameters
+    ----------
+    variables: [tf.Tensor]
+        List of scalar tensors.
+    Returns
+    -------
+    averages: [tf.Tensor]
+        List of scalar tensors corresponding to averages
+        of al the `variables` (in order)
+    apply_op: tf.runnable
+        Op to be run to update the averages with current value
+        of variables.
+    """
+    averager = tf.train.ExponentialMovingAverage(decay=decay)
+    apply_op = averager.apply(variables)
+    return [averager.average(v) for v in variables], apply_op
+
+def minimize_and_clip(optimizer, objective, var_list, clip_val=10):
+    """Minimized `objective` using `optimizer` w.r.t. variables in
+    `var_list` while ensure the norm of the gradients for each
+    variable is clipped to `clip_val`
+    """
+    gradients = optimizer.compute_gradients(objective, var_list=var_list)
+    for i, (grad, var) in enumerate(gradients):
+        if grad is not None:
+            gradients[i] = (tf.clip_by_norm(grad, clip_val), var)
+    return optimizer.apply_gradients(gradients)
+
+def initialize_interdependent_variables(session, vars_list, feed_dict):
+    """Initialize a list of variables one at a time, which is useful if
+    initialization of some variables depends on initialization of the others.
+    """
+    vars_left = vars_list
+    while len(vars_left) > 0:
+        new_vars_left = []
+        for v in vars_left:
+            try:
+                # If using an older version of TensorFlow, uncomment the line
+                # below and comment out the line after it.
+		#session.run(tf.initialize_variables([v]), feed_dict)
+                session.run(tf.variables_initializer([v]), feed_dict)
+            except tf.errors.FailedPreconditionError:
+                new_vars_left.append(v)
+        if len(new_vars_left) >= len(vars_left):
+            # This can happend if the variables all depend on each other, or more likely if there's
+            # another variable outside of the list, that still needs to be initialized. This could be
+            # detected here, but life's finite.
+            raise Exception("Cycle in variable dependencies, or extenrnal precondition unsatisfied.")
+        else:
+            vars_left = new_vars_left
+
+def get_wrapper_by_name(env, classname):
+    currentenv = env
+    while True:
+        if classname in currentenv.__class__.__name__:
+            return currentenv
+        elif isinstance(env, gym.Wrapper):
+            currentenv = currentenv.env
+        else:
+            raise ValueError("Couldn't find wrapper named %s"%classname)
+
+class ReplayBuffer(object):
+    def __init__(self, size, frame_history_len):
+        """This is a memory efficient implementation of the replay buffer.
+
+        The sepecific memory optimizations use here are:
+            - only store each frame once rather than k times
+              even if every observation normally consists of k last frames
+            - store frames as np.uint8 (actually it is most time-performance
+              to cast them back to float32 on GPU to minimize memory transfer
+              time)
+            - store frame_t and frame_(t+1) in the same buffer.
+
+        For the tipical use case in Atari Deep RL buffer with 1M frames the total
+        memory footprint of this buffer is 10^6 * 84 * 84 bytes ~= 7 gigabytes
+
+        Warning! Assumes that returning frame of zeros at the beginning
+        of the episode, when there is less frames than `frame_history_len`,
+        is acceptable.
+
+        Parameters
+        ----------
+        size: int
+            Max number of transitions to store in the buffer. When the buffer
+            overflows the old memories are dropped.
+        frame_history_len: int
+            Number of memories to be retried for each observation.
+        """
+        self.size = size
+        self.frame_history_len = frame_history_len
+
+        self.next_idx      = 0
+        self.num_in_buffer = 0
+
+        self.obs      = None
+        self.action   = None
+        self.reward   = None
+        self.done     = None
+
+    def can_sample(self, batch_size):
+        """Returns true if `batch_size` different transitions can be sampled from the buffer."""
+        return batch_size + 1 <= self.num_in_buffer
+
+    def _encode_sample(self, idxes):
+        obs_batch      = np.concatenate([self._encode_observation(idx)[None] for idx in idxes], 0)
+        act_batch      = self.action[idxes]
+        rew_batch      = self.reward[idxes]
+        next_obs_batch = np.concatenate([self._encode_observation(idx + 1)[None] for idx in idxes], 0)
+        done_mask      = np.array([1.0 if self.done[idx] else 0.0 for idx in idxes], dtype=np.float32)
+
+        return obs_batch, act_batch, rew_batch, next_obs_batch, done_mask
+
+
+    def sample(self, batch_size):
+        """Sample `batch_size` different transitions.
+
+        i-th sample transition is the following:
+
+        when observing `obs_batch[i]`, action `act_batch[i]` was taken,
+        after which reward `rew_batch[i]` was received and subsequent
+        observation  next_obs_batch[i] was observed, unless the epsiode
+        was done which is represented by `done_mask[i]` which is equal
+        to 1 if episode has ended as a result of that action.
+
+        Parameters
+        ----------
+        batch_size: int
+            How many transitions to sample.
+
+        Returns
+        -------
+        obs_batch: np.array
+            Array of shape
+            (batch_size, img_h, img_w, img_c * frame_history_len)
+            and dtype np.uint8
+        act_batch: np.array
+            Array of shape (batch_size,) and dtype np.int32
+        rew_batch: np.array
+            Array of shape (batch_size,) and dtype np.float32
+        next_obs_batch: np.array
+            Array of shape
+            (batch_size, img_h, img_w, img_c * frame_history_len)
+            and dtype np.uint8
+        done_mask: np.array
+            Array of shape (batch_size,) and dtype np.float32
+        """
+        assert self.can_sample(batch_size)
+        idxes = sample_n_unique(lambda: random.randint(0, self.num_in_buffer - 2), batch_size)
+        return self._encode_sample(idxes)
+
+    def encode_recent_observation(self):
+        """Return the most recent `frame_history_len` frames.
+
+        Returns
+        -------
+        observation: np.array
+            Array of shape (img_h, img_w, img_c * frame_history_len)
+            and dtype np.uint8, where observation[:, :, i*img_c:(i+1)*img_c]
+            encodes frame at time `t - frame_history_len + i`
+        """
+        assert self.num_in_buffer > 0
+        return self._encode_observation((self.next_idx - 1) % self.size)
+
+    def _encode_observation(self, idx):
+        end_idx   = idx + 1 # make noninclusive
+        start_idx = end_idx - self.frame_history_len
+        # this checks if we are using low-dimensional observations, such as RAM
+        # state, in which case we just directly return the latest RAM.
+        if len(self.obs.shape) == 2:
+            return self.obs[end_idx-1]
+        # if there weren't enough frames ever in the buffer for context
+        if start_idx < 0 and self.num_in_buffer != self.size:
+            start_idx = 0
+        for idx in range(start_idx, end_idx - 1):
+            if self.done[idx % self.size]:
+                start_idx = idx + 1
+        missing_context = self.frame_history_len - (end_idx - start_idx)
+        # if zero padding is needed for missing context
+        # or we are on the boundry of the buffer
+        if start_idx < 0 or missing_context > 0:
+            frames = [np.zeros_like(self.obs[0]) for _ in range(missing_context)]
+            for idx in range(start_idx, end_idx):
+                frames.append(self.obs[idx % self.size])
+            return np.concatenate(frames, 2)
+        else:
+            # this optimization has potential to saves about 30% compute time \o/
+            img_h, img_w = self.obs.shape[1], self.obs.shape[2]
+            return self.obs[start_idx:end_idx].transpose(1, 2, 0, 3).reshape(img_h, img_w, -1)
+
+    def store_frame(self, frame):
+        """Store a single frame in the buffer at the next available index, overwriting
+        old frames if necessary.
+
+        Parameters
+        ----------
+        frame: np.array
+            Array of shape (img_h, img_w, img_c) and dtype np.uint8
+            the frame to be stored
+
+        Returns
+        -------
+        idx: int
+            Index at which the frame is stored. To be used for `store_effect` later.
+        """
+        if self.obs is None:
+            self.obs      = np.empty([self.size] + list(frame.shape), dtype=np.uint8)
+            self.action   = np.empty([self.size],                     dtype=np.int32)
+            self.reward   = np.empty([self.size],                     dtype=np.float32)
+            self.done     = np.empty([self.size],                     dtype=np.bool)
+        self.obs[self.next_idx] = frame
+
+        ret = self.next_idx
+        self.next_idx = (self.next_idx + 1) % self.size
+        self.num_in_buffer = min(self.size, self.num_in_buffer + 1)
+
+        return ret
+
+    def store_effect(self, idx, action, reward, done):
+        """Store effects of action taken after obeserving frame stored
+        at index idx. The reason `store_frame` and `store_effect` is broken
+        up into two functions is so that once can call `encode_recent_observation`
+        in between.
+
+        Paramters
+        ---------
+        idx: int
+            Index in buffer of recently observed frame (returned by `store_frame`).
+        action: int
+            Action that was performed upon observing this frame.
+        reward: float
+            Reward that was received when the actions was performed.
+        done: bool
+            True if episode was finished after performing that action.
+        """
+        self.action[idx] = action
+        self.reward[idx] = reward
+        self.done[idx]   = done
+
