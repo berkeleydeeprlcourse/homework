b2e1b084caffb7226174a6ce96fa4d9dd437d8b1
Holly Grimm <holly.grimm@gmail.com>
2018-06-22 07:15:42 -0600

    create function to generate rollout data

--- hw1/run_expert.py
+++ hw1/run_expert.py
@@ -8,40 +8,43 @@ Example usage:
 
 Author of this script and included expert policies: Jonathan Ho (hoj@openai.com)
 """
-
+import os
 import pickle
 import tensorflow as tf
 import numpy as np
 import tf_util
 import gym
+from gym import wrappers
 import load_policy
 
-def main():
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('expert_policy_file', type=str)
-    parser.add_argument('envname', type=str)
-    parser.add_argument('--render', action='store_true')
-    parser.add_argument("--max_timesteps", type=int)
-    parser.add_argument('--num_rollouts', type=int, default=20,
-                        help='Number of expert roll outs')
-    args = parser.parse_args()
+def generate_all_rollout_data():
+    generate_rollout_data('experts/Ant-v1.pkl', 'Ant-v1', 250, False, 'data')
+    generate_rollout_data('experts/HalfCheetah-v1.pkl', 'HalfCheetah-v1', 10, False, 'data')
+    generate_rollout_data('experts/Hopper-v1.pkl', 'Hopper-v1', 10, False, 'data')
+    generate_rollout_data('experts/Humanoid-v1.pkl', 'Humanoid-v1', 250, False, 'data')
+    generate_rollout_data('experts/Reacher-v1.pkl', 'Reacher-v1', 250, False, 'data')
+    generate_rollout_data('experts/Walker2d-v1.pkl', 'Walker2d-v1', 10, False, 'data') 
 
+
+def generate_rollout_data(expert_policy_file, env_name, num_rollouts, render, output_dir=None, save=False, max_timesteps=None):
     print('loading and building expert policy')
-    policy_fn = load_policy.load_policy(args.expert_policy_file)
+    policy_fn = load_policy.load_policy(expert_policy_file)
     print('loaded and built')
 
     with tf.Session():
         tf_util.initialize()
 
-        import gym
-        env = gym.make(args.envname)
-        max_steps = args.max_timesteps or env.spec.timestep_limit
+        env = gym.make(env_name)
+        max_steps = max_timesteps or env.spec.timestep_limit
 
+        if save:
+            expert_results_dir = os.path.join(os.getcwd(), 'results', env_name, 'expert')
+            env = wrappers.Monitor(env, expert_results_dir, force=True)
+        
         returns = []
         observations = []
         actions = []
-        for i in range(args.num_rollouts):
+        for i in range(num_rollouts):
             print('iter', i)
             obs = env.reset()
             done = False
@@ -54,7 +57,7 @@ def main():
                 obs, r, done, _ = env.step(action)
                 totalr += r
                 steps += 1
-                if args.render:
+                if render:
                     env.render()
                 if steps % 100 == 0: print("%i/%i"%(steps, max_steps))
                 if steps >= max_steps:
@@ -66,7 +69,26 @@ def main():
         print('std of return', np.std(returns))
 
         expert_data = {'observations': np.array(observations),
-                       'actions': np.array(actions)}
+                       'actions': np.array(actions),
+                       'mean_return': np.mean(returns),
+                        'std_return': np.std(returns)}
+
+        if output_dir is not 'None':
+            output_dir = os.path.join(os.getcwd(), output_dir)
+            filename = '{}_data_{}_rollouts.pkl'.format(env_name, num_rollouts)
+            with open(output_dir + '/' + filename,'wb') as f:
+                 pickle.dump(expert_data, f)
 
 if __name__ == '__main__':
-    main()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('expert_policy_file', type=str)
+    parser.add_argument('envname', type=str)
+    parser.add_argument("--max_timesteps", type=int)
+    parser.add_argument('--num_rollouts', type=int, default=20,
+                        help='Number of expert roll outs')
+    parser.add_argument('--render', action='store_true')
+    parser.add_argument("--output_dir", type=str, default='data')
+    args = parser.parse_args()
+
+    generate_rollout_data(args.expert_policy_file, args.envname, args.num_rollouts, args.render, args.max_timesteps)
