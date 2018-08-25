0f36463d16aafd3f7ff8ae4397d2ae7e54883076
Jiahao Yao <zxc604036406@foxmail.com>
2018-08-25 11:33:20 -0700

    typo

--- hw1/demo.bash
+++ hw1/demo.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eux
-for e in Hopper-v1 Ant-v1 HalfCheetah-v1 Humanoid-v1 Reacher-v1 Walker2d-v1
+for e in Hopper-v2 Ant-v2 HalfCheetah-v2 Humanoid-v2 Reacher-v2 Walker2d-v2
 do
     python run_expert.py experts/$e.pkl $e --render --num_rollouts=1
 done
