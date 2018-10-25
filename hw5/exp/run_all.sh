e87f386c110b4d786b345ef68a75798e672495c5
Michael Chang <95changmichael13@gmail.com>
2018-10-25 08:10:50 -0700

    fix hyperparams for halfcheetah

--- hw5/exp/run_all.sh
+++ hw5/exp/run_all.sh
@@ -23,4 +23,5 @@ python train_ac_exploration_f18.py PointMass-v0 -n 100 -b 1000 -e 3 --density_mo
 ###    P4 HalfCheetah   ###
 ###########################
 python train_ac_exploration_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 30000 -lr 0.02 --density_model none --exp_name HC_bc0
-python train_ac_exploration_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 30000 -lr 0.02 --density_model ex2 -bc 0.001 -kl 0.1 -dlr 0.005 --exp_name HC_bc0.001_kl0.1_dlr0.005
+python train_ac_exploration_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 30000 -lr 0.02 --density_model ex2 -bc 0.001 -kl 0.1 -dlr 0.005 -dti 1000 --exp_name HC_bc0.001_kl0.1_dlr0.005_dti1000
+python train_ac_exploration_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 30000 -lr 0.02 --density_model ex2 -bc 0.0001 -kl 0.1 -dlr 0.005 -dti 10000 --exp_name HC_bc0.001_kl0.1_dlr0.005_dti10000
