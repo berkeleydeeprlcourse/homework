d048689012521b84323b5d83c1ddc8b286dba5f3
Michael Chang <95changmichael13@gmail.com>
2018-11-05 10:47:59 -0800

    fixed wrong version of run_all.sh

--- hw5/exp/run_all.sh
+++ hw5/exp/run_all.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# TODO: change logger name
-
 ##########################
 ### P1 Hist PointMass  ###
 ##########################
@@ -19,11 +17,11 @@ python train_ac_exploration_f18.py PointMass-v0 -n 100 -b 1000 -e 3 --density_mo
 ###  P3 EX2 PointMass  ###
 ##########################
 
-python train_ac_exploration_f18.py PointMass-v0 -n 100 -b 1000 -e 3 --density_model ex2 -s 8 -bc 0.001 -kl 0.1 -dlr 0.001 -dh 8 -dti 1000 --exp_name PM_ex2_s8_bc0.001_kl0.1_dlr0.001_dh8_dti1000
+python train_ac_exploration_f18.py PointMass-v0 -n 100 -b 1000 -e 3 --density_model ex2 -s 8 -bc 0.05 -kl 0.1 -dlr 0.001 -dh 8 -dti 1000 --exp_name PM_ex2_s8_bc0.05_kl0.1_dlr0.001_dh8_dti1000
 
 ###########################
 ###    P4 HalfCheetah   ###
 ###########################
 python train_ac_exploration_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 30000 -lr 0.02 --density_model none --exp_name HC_bc0
-# python train_ac_exploration_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 30000 -lr 0.02 --density_model rbf -bc 0.001 -sig 0.2 --exp_name HC_bc0.001_sig0.2
-python train_ac_exploration_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 30000 -lr 0.02 --density_model ex2 -bc 0.001 -kl 0.1 -dlr 0.005 --exp_name HC_bc0.001_kl0.1_dlr0.005
+python train_ac_exploration_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 30000 -lr 0.02 --density_model ex2 -bc 0.001 -kl 0.1 -dlr 0.005 -dti 1000 --exp_name HC_bc0.001_kl0.1_dlr0.005_dti1000
+python train_ac_exploration_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 3 -l 2 -s 32 -b 30000 -lr 0.02 --density_model ex2 -bc 0.0001 -kl 0.1 -dlr 0.005 -dti 10000 --exp_name HC_bc0.001_kl0.1_dlr0.005_dti10000
