45c24beb6f310e3b77edc69363e753b42ef9ec90
Abdelrahman Ogail <ogail@marble.io>
2019-04-03 07:24:54 -0700

    Update dagger script to enumerate all envs

--- hw1/dagger.sh
+++ hw1/dagger.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -eux
 RUN_ID=`date '+%Y_%m_%d__%H_%M_%S'`
-# for e in Hopper-v2 Ant-v2 HalfCheetah-v2 Reacher-v2 Walker2d-v2 Humanoid-v2
-for e in Humanoid-v2
+for e in Hopper-v2 Ant-v2 HalfCheetah-v2 Reacher-v2 Walker2d-v2 Humanoid-v2
 do
     # generate expert data
     python3 run_sim.py experts/$e.pkl $e --num-rollouts 10 --expert --output $RUN_ID/$e \
